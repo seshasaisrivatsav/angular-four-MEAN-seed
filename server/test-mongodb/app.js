7267dde57fae8abcfcbfa8a3357c50ddf6a4633a
sesha sai srivatsav kuchibhotla <sesha@seshas-MacBook-Pro.local>
2017-07-27 16:48:02 -0400

    updated connection string

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -8,17 +8,17 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+    var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+    if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+      var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+      var password = process.env.MLAB_PASSWORD_WEBDEV;
+      connectionString = 'mongodb://' + username + ':' + password;
+      connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
     }
 
-    var mongoose = require("mongoose");
+
+  var mongoose = require("mongoose");
     mongoose.connect(connectionString);
 
     var TestSchema = mongoose.Schema({
