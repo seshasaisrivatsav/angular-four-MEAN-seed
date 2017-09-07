87491eb97a8d36132931ab2c727268d3bf00c9c2
Sesha Sai Srivatsav Kuchibhotla <srivatsav@Seshas-MacBook-Pro.local>
2017-09-07 19:57:02 -0400

    updated test-mongodb/app.js

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -9,10 +9,20 @@ module.exports = function(app)
     app.delete("/api/test/:id", deleteMessage);
 
 
-  var connectionString = 'mongodb://webappmaker:webappmaker@ds163181.mlab.com:63181/webappmaker';
+  var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
+
+  if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds163181.mlab.com:63181/webappmaker'; // user yours
+  }
+
 
   var mongoose = require("mongoose");
-    mongoose.connect(connectionString);
+    mongoose.connect(connectionString, {
+      useMongoClient: true
+    });
 
     var TestSchema = mongoose.Schema({
         message: String
